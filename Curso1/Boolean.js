ca8c2f905341d4dc9d7100e174a90b9a64750adb
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-08-22 13:34:18 -0300

    new

---
+++ Curso1/Boolean.js
@@ -0,0 +1,15 @@
+const num1 = 2;
+const num2 = 2;
+const num3 = 3;
+const result = num1 === num2;
+const str1 = 'Pedro';
+const str2 = 'pedro';
+console.log('comparações de str')
+console.log(str1 === str2);
+console.log(str1.toLocaleLowerCase() === str2);
+
+
+console.log('comparações númericas.');
+console.log(num1 === num3);
+console.log(num1 === num2);
+console.log(result);
