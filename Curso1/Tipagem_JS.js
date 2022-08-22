32d2a4acf34b51c4f704be2d3a919d60fec987a0
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-08-22 15:16:21 -0300

    new

---
+++ Curso1/Tipagem_JS.js
@@ -0,0 +1,8 @@
+let tip;
+tip = 22;
+tip = '12223';
+tip = true; // tipagem dinâmica, ela identifica e troca automaticamente.
+x = 'oi';   // << sem declaração exposta também. 
+console.log(x);
+
+     
