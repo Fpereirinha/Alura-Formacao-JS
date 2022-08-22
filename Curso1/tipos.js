f12f827478d58787e01d361274a380f146d47654
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-08-22 13:08:34 -0300

    new

---
+++ Curso1/tipos.js
@@ -0,0 +1,25 @@
+// tipos de váriaveis em js
+const meuNumero = 3;
+
+const num1 = 1;
+const num2 = 2;
+const soma = num1 + num2;
+
+console.log(soma);
+console.log(num1-num2);
+console.log(num1/num2);
+console.log(num1*num2);
+// floats
+const f1 = .5;
+const f2 = 2.2;
+const i1 = 3;
+
+console.log(f1-f2)
+console.log(f1*i1)
+console.log(f1+f2+i1)
+
+// NaN    ->  not a number   // Declaração incorreta, ausência de conversão.
+
+const real = 1;
+const str_v = 'alura';
+console.log(real * str_v)
