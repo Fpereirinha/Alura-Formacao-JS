bb2fe302c735e924be39a93842d080e44bbf8656
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-08-22 16:12:25 -0300

    new

---
+++ Curso1/OperadorTernario.js
@@ -0,0 +1,9 @@
+let idade_minina = 18;
+let idade_cliente = 19;
+
+/* if (idade_cliente >= idade_minina){
+    console.log('Pode beber.')
+} else{
+    console.log('Não pode beber.')
+}*/
+console.log(idade_cliente>=idade_minina ? 'Pode beber' : 'Não pode beber') // ternário, if true / false
