cfc59e9e0a4445f6397a5551a6abcb3eb232ee63
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-08-22 19:50:43 -0300

    new

---
+++ Curso2/Slice.js
@@ -0,0 +1,4 @@
+const nomes = ['Pedro', 'Eleisio', 'Josimar', 'Lucas', 'Thiago', 'Gabriel', 'Jéssica', 'Bianca', 'Barbara', 'Leonardo'];
+const sala1 = nomes.slice(0, (nomes.length/2))
+const sala2 = nomes.slice((nomes.length / 2))
+console.log(`Sala 1: ${sala1} \n Sala 2: ${sala2}`)
