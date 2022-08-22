2b027c94d8d3e3a7486b27ac645fb85c0e33f9b9
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-08-22 20:13:55 -0300

    new

---
+++ Curso2/Uniao.js
@@ -0,0 +1,5 @@
+const alunos_python = ['Pedro', 'Alisson', 'Joao', 'Rodrigo']
+const alunos_js = ['Thiago', 'Josimar', 'Raquel', 'Jenifer']
+
+const sala_conjunta = alunos_js.concat(alunos_python)
+console.log(sala_conjunta)
