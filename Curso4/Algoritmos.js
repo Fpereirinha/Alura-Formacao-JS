d1c736e54e920c4e42d1f39d41a53915bc6c8cff
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-08-24 12:39:41 -0300

    update

--- Curso4/Algoritmos.js
+++ Curso4/Algoritmos.js
@@ -1,26 +1,27 @@
-let lista = [1,55,11,22,44,9,9,0,0,0,2]
+let lista = [1,55,11,22,44,9,9,0,0,0,55,2]
 let lista_ordenada = []
+let lista_ordenada2 = []
 // organizar sem repetir //
 for (let i = 0; i < lista.length; i++) {
     let menor = 9999999999
     for (const number of lista) {
         if (number <= menor && ! lista_ordenada.includes(number)) {menor = number}
     }
-    if (! lista_ordenada.includes(menor) && menor !== 9999999999){lista_ordenada.push(menor)}
+    if (menor !== 9999999999){lista_ordenada.push(menor)}
 }
 console.log(lista_ordenada)
 console.log(lista)
 // organizar, repetindo//
-let tamanho = lista.length
-for (let i = 0; i < tamanho; i++) {
-    let menor = lista[0]
-    for (const number of lista) {
+copy_list = [...lista]
+for (let i = 0; i < lista.length; i++) {
+    let menor = copy_list[0]
+    for (const number of copy_list) {
         if (number <= menor) {menor = number}
     }
-    lista_ordenada.push(menor)
-    x = lista.indexOf(menor)
-    lista.splice(x,1)
+    lista_ordenada2.push(menor)
+    x = copy_list.indexOf(menor)
+    copy_list.splice(x,1)
 }
-
 console.log(lista)
-console.log(lista_ordenada)
+console.log(lista_ordenada2)
+
