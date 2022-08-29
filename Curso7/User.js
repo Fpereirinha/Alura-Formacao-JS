20e4c941a336c1d04f9c2c5fe13dd2a4fa048d84
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-08-29 18:48:48 -0300

    update

--- Curso7/User.js
+++ Curso7/User.js
@@ -1,14 +1,57 @@
 export default class User{
     #nome
-    constructor(nome, email, nascimento, role = 'estudante', ativo = true) {
+    #email
+    #nascimento
+    #role
+    #ativo
+    #sobrenome
+    constructor(nome,sobrenome, email, nascimento, role = 'estudante', ativo = true) {
         this.#nome = nome
-        this.email = email
-        this.nascimento = nascimento
-        this.role = role
-        this.ativo = ativo
+        this.#sobrenome = sobrenome
+        this.#email = email
+        this.#nascimento = nascimento
+        this.#role = role
+        this.#ativo = ativo
     }
+    // getters //
+    get nome(){
+        return `${this.#nome} ${this.#sobrenome}`
+    }
+    get email(){
+        return this.#email
+    }
+    get nascimento(){
+        return this.#nascimento
+    }
+    get role(){
+        return this.#role
+    }
+    get ativo(){
+        return this.#ativo
+    }
+    // getters //
+    // setters //
+    set nome(nome_){
+        let [nome__, ...sobrenome] = nome_.split(" ")
+        sobrenome = sobrenome.join(' ')
+        this.#sobrenome = sobrenome
+        this.#nome = nome__
+    }
+    set email(email_){
+        this.#email = email_
+    }
+    set role(role_){
+        this.#role = role_
+    }
+    set ativo(ativo_){
+        this.#ativo = ativo_
+    }
+    set nascimento(nascimento_){
+        this.#nascimento = nascimento_
+    }
+    // setters //
     info(){
-        return `${this.#nome}  ${this.email} ---- ${this.role}`
+        return `${this.nome}  ${this.nascimento} ---- ${this.role}`
     }
 }
 
--- Curso7/index.js
+++ Curso7/index.js
@@ -1,3 +1,17 @@
 import User from "./User.js";
 import Docente from "./Docente.js";
 import Admin from "./Admin.js";
+const Pedro = new User('Pedro','Fernandes Pereira','@gmail.com','06.09.2000')
+console.log(Pedro.info())
+
+const Pedro_adm = new Admin('Pedroadm','Fernandes Pereira', '@gmail.com','06.09.2000')
+console.log(Pedro_adm.info())
+console.log(Pedro_adm.nome)
+console.log(Pedro_adm)
+
+Pedro_adm.nome = 'Pedro_Administador Junior'
+console.log(Pedro_adm.nome)
+/*
+let [nome, ...sobrenome] = 'Pedro Fernandes Pereira'.split(' ')
+console.log(nome)
+console.log(sobrenome.join(' '))*/
